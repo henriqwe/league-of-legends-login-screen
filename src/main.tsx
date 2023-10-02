bba42b4bd5c5f53758c25ab318c6e563b42dca36
Claudio Henrique <henriqwe96@gmail.com>
2023-10-02 13:36:55 -0300

    chore: style configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/index.ts'
+import { GlobalStyle } from './styles/globalStyle.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 )
